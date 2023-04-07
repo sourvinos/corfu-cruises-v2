96cd9d0139361e857758424ffd56eed52eb97a73
John Sourvinos <johnsourvinos@hotmail.com>
2023-04-07 11:23:18 +0300

    Shows loader on login

--- FrontEnd/src/app/shared/services/helper.service.ts
+++ FrontEnd/src/app/shared/services/helper.service.ts
@@ -9,6 +9,21 @@ import { MessageLabelService } from './message-label.service'
 import { ModalActionResultService } from './modal-action-result.service'
 import { SessionStorageService } from './session-storage.service'
 import { environment } from 'src/environments/environment'
+import { Observable, Subject, defer, finalize } from 'rxjs'
+
+export function prepare<T>(callback: () => void): (source: Observable<T>) => Observable<T> {
+    return (source: Observable<T>): Observable<T> => defer(() => {
+        callback()
+        return source
+    })
+}
+
+export function indicate<T>(indicator: Subject<boolean>): (source: Observable<T>) => Observable<T> {
+    return (source: Observable<T>): Observable<T> => source.pipe(
+        prepare(() => indicator.next(true)),
+        finalize(() => indicator.next(false))
+    )
+}
 
 @Injectable({ providedIn: 'root' })
 
